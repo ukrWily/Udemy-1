61a658cb5358f1d3c355f14245498791b6efd417
”ukrWily” <ukrwily@gmail.com>
2023-12-23 20:45:39 +0200

    added two variables with promt and added their values to object personalMovieDB.movies

--- js/script.js
+++ js/script.js
@@ -31,6 +31,7 @@ let numberOfFilms = prompt('Сколько фильмов вы уже посмо
 //     - actors - тоже поместить пустой объект
 //     - genres - сюда поместить пустой массив
 //     - privat - в это свойство поместить boolean(логическое) значение false
+
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -38,3 +39,17 @@ const personalMovieDB = {
   genres: [],
   privat: false
 }
+
+// 3) Задайте пользователю по два раза вопросы:
+//     - 'Один из последних просмотренных фильмов?'
+//     - 'На сколько оцените его?'
+// Ответы стоит поместить в отдельные переменные
+// Записать ответы в объект movies в формате: 
+//     movies: {
+//         'logan': '8.1'
+//     }
+
+let lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+let ratingMovie = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies.lastMovie = ratingMovie;
