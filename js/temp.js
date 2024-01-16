2570ef04aca3b8dcd83e6f761263de8310477645
”ukrWily” <ukrwily@gmail.com>
2024-01-16 19:50:52 +0200

    Added two learning tasks

--- js/temp.js
+++ js/temp.js
@@ -315,6 +315,7 @@
 // transferWaitors(restorantData);
 
 // <<<<<<<<<<<<    ===========================================================================================    >>>>>>>>>>>>
+//  Factorial through recursion
 
 // function factorial(num) {
 //     if (Number(num) !== num || num % 1 !== 0) {
@@ -327,3 +328,118 @@
 // }
 
 // console.log(factorial(5));
+
+// <<<<<<<<<<<<    ================================================    >>>>>>>>>>>>
+/*
+Если на входе функции summary = 25, то на результат должен быть 17. Всего в числах от 1 до 17 содержится 25 цифр: 1234567891011121314151617.
+
+Функция на вход как раз принимает это общее количество цифр, а возвращает конечное число, то есть последнюю страницу книги.
+
+        amountOfPages(5) => 5
+        amountOfPages(25) => 17
+        amountOfPages(1095) => 401   
+        amountOfPages(185) => 97
+*/
+
+// function amountOfPages(summary){
+//   let stringNumber = '';
+//   let length = 1;
+//   let i = 1;
+//   while (true){
+//     if (length >= summary){
+//       break;
+//     } else {
+//       stringNumber += i;
+//       length = stringNumber.length;
+//       i++;
+//     }
+
+//     // console.log(length);
+//   }
+//   return i - 1;
+// }
+// console.log(amountOfPages(1));
+
+// teacher's solution
+// function amountOfPages(summary){
+//     let result = '';
+//     let n = 0;
+
+//     for (let i = 1; i <= summary; i++) {
+//       result += i;
+//       if (result.length === summary) {
+//         n = i;
+//         break;
+//       }
+//     }
+
+//     return n;
+//   } 
+
+// <<<<<<<<<<<<    =====================================    >>>>>>>>>>>>
+// Панграмма — это предложение, в котором каждая буква алфавита встречается хотя бы по одному разу по возможности без повторений. Например, предложение «The quick brown fox jumps over the lazy dog» является панграммой, поскольку в нем хотя бы один раз используются буквы от A до Z (регистр значения не имеет).
+
+// Напишите функцию isPangram, которая принимает в себя строку и возвращает логическое значение. Если строка является панграммой - вернется true, если нет - false.
+
+// Пример:
+
+// isPangram(«The quick brown fox jumps over the lazy dog») => true
+
+// isPangram(«Hello world») => false
+
+// function isPangram(string) {
+//   let symbols = new Set();
+//   for (let i = 0; i < string.length; i++) {
+//     const letter = string[i].toLowerCase();
+//     if (letter.charCodeAt() > 96 && letter.charCodeAt() < 123) {
+//       symbols.add(letter);
+//     }
+//   }
+//   return symbols.size == 26;
+// }
+// console.log(isPangram('The brown fox jumps over the lazy dog'));
+
+// // teacher's solutions
+
+// // Вариант, когда строка переводится в нижний регистр до всех операций только 1 раз
+// // Это должно экономить ресурсы компьютера
+// function isPangram(string){
+//     string = string.toLowerCase();
+//     return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
+//       return string.indexOf(x) !== -1;
+//     });
+// }
+
+// // С другим методом и строка каждый раз преобразовывается в коллбэке
+// function isPangram(string){
+//     return 'abcdefghijklmnopqrstuvwxyz'
+//       .split('')
+//       .every((x) => string.toLowerCase().includes(x));
+// }
+
+// // При помощи цикла
+// function isPangram(str) {
+//     letters: for (var c = 0; c < 26; c++) {
+//         for (let i = 0; i < str.length; i++) {
+//             let s = str.charCodeAt(i)
+//             if (s < 65 || s > 90 && s < 97 || s > 122) continue
+//             if (s === 65 + c || s === 97 + c) continue letters
+//         }
+      
+//         return false
+//     }
+    
+//     return true
+// }
+
+// // При помощи Set
+// function isPangram(string) {
+//     return new Set(string.toLocaleLowerCase().replace(/[^a-z]/gi, '').split('')).size === 26;
+// }
+
+// // С использованием регулярных выражений
+// function isPangram(string){
+//     return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
+// }
+
+// <<<<<<<<<<<<    =====================================================    >>>>>>>>>>>>
